d5c4c1a577ec49ad0c60c1f96b5895e74a31890d
kevileem2 <32266918+kevileem2@users.noreply.github.com>
2017-10-17 10:01:22 +0200

    Added files for assignement 3

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $x = 7;
+    $y = 10;
+    echo "$y + $x = " . ($x + $y) . "<br>";
+    echo "$y - $x = " . ($y - $x) . "<br>";
+    echo "$y * $x = " . ($y * $x) . "<br>";
+    echo "$y / $x = " . ($y / $x) . "<br>";
+    echo "$y % $x = " . ($y % $x) . "<br>";
+    ?>
+</body>
+</html>
